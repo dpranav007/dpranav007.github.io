f000b14331c2fd9035907a97c6194972d0e89208
Pranav <dpranav007@gmail.com>
2020-06-01 06:12:14 +0530

    improved

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d194159fc0d8ab6cefcab1a888a2ea13.js"
+  "/precache-manifest.6ebeef418207f610a972be9ed22ad565.js"
 );
 
 self.addEventListener('message', (event) => {
