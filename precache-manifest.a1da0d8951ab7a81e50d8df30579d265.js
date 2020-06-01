c80c8ba10f059a82648ad3f75401307355b05808
Pranav <dpranav007@gmail.com>
2020-06-01 05:51:50 +0530

    improved

---
+++ precache-manifest.a1da0d8951ab7a81e50d8df30579d265.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2475c5f9b552c422b003f5c2ef1494fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f86141ea621673f8c014",
+    "url": "/static/css/main.8ebfd5b2.chunk.css"
+  },
+  {
+    "revision": "ac2dabc2c3e2eefd3073",
+    "url": "/static/js/2.77d80d1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.77d80d1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f86141ea621673f8c014",
+    "url": "/static/js/main.258dff14.chunk.js"
+  },
+  {
+    "revision": "485927d523f72ea2f4ff",
+    "url": "/static/js/runtime-main.f394aa81.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "e0bd96dc51b3832dfc08f3bee1bd7ebc",
+    "url": "/static/media/Busflix.e0bd96dc.e0bd96dc.pdf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e47c3e26372c670c4a4eeac6eea0bf98",
+    "url": "/static/media/airbnbLogo.e47c3e26.jpg"
+  },
+  {
+    "revision": "8e8b350ad04e589b2d66f24ce55a445f",
+    "url": "/static/media/boltpics.8e8b350a.jpg"
+  },
+  {
+    "revision": "9d6452042c8a292e489a2ebe1caa3e11",
+    "url": "/static/media/busflix2.9d645204.jpg"
+  },
+  {
+    "revision": "6203e44d6bc71fc2175011e8736d02b0",
+    "url": "/static/media/canstores.6203e44d.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "0dc4c4a330902d96333f6931a322324b",
+    "url": "/static/media/qsso.0dc4c4a3.png"
+  },
+  {
+    "revision": "c971005a69829df144f625d78336cc5d",
+    "url": "/static/media/robocon_2013.c971005a.jpg"
+  },
+  {
+    "revision": "46d9f22baccdf7718ee608f0b5503c6c",
+    "url": "/static/media/undraw_coding_6mjf.46d9f22b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.faf2f46d8d2bedb006ad08880ee257e8.js"
+  "/precache-manifest.a1da0d8951ab7a81e50d8df30579d265.js"
 );
 
 self.addEventListener('message', (event) => {
